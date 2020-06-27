1e18c55a4fe0cb28ad4b316dca82a0ea25ebace9
patrickz98 <patrickz98@users.noreply.github.com>
2020-06-27 13:59:01 +0200

    Check in.

--- exercise.06/main.py
+++ exercise.06/main.py
@@ -1,6 +1,6 @@
 import math
 import random
-from typing import List
+from typing import List, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -32,13 +32,73 @@ def generate_distribution_points(center: (float, float), variance: (float, float
     return x_points, y_points
 
 
+def k_mean(k: int, training_data: List[Tuple[float, float, int]]) -> List[Tuple[float, float]]:
+    centroids = []
+    c_values = []
+
+    for _ in range(k):
+        x = random.uniform(-5, 5)
+        y = random.uniform(-5, 5)
+        centroids.append((x, y))
+        c_values.append([])
+
+    for inx in range(len(training_data)):
+        x = training_data[inx]
+        best_distance = -1
+        best_centroid = -1
+
+        for iny in range(k):
+            centroid = centroids[iny]
+            distance = (x[0] - centroid[0]) ** 2 + (x[1] - centroid[1]) ** 2
+
+            if best_distance < 0 or best_distance > distance:
+                best_distance = distance
+                best_centroid = iny
+
+        c_values[best_centroid].append(x)
+
+    print(c_values)
+
+    for inx in range(k):
+        x_sum = 0
+        y_sum = 0
+
+        count = len(c_values[inx])
+
+        for x in c_values[inx]:
+            x_sum += x[0]
+            y_sum += x[1]
+
+        centroids[inx] = (x_sum / count, y_sum / count)
+
+    return centroids
+
+
 random.seed(19980528)
-x_points_1, y_points_1 = generate_distribution_points((5, -5), (1.3, 0.9))
+x_points_1, y_points_1 = generate_distribution_points((5, -5), (0.9, 0.9))
 x_points_2, y_points_2 = generate_distribution_points((-5, 2), (1.4, 1.4))
-x_points_3, y_points_3 = generate_distribution_points((0, 6),  (1.0, 0.8))
+x_points_3, y_points_3 = generate_distribution_points((0, 5), (0.8, 0.8))
 
 plt.figure(0)
 plt.scatter(x_points_1, y_points_1)
 plt.scatter(x_points_2, y_points_2)
 plt.scatter(x_points_3, y_points_3)
+# plt.savefig("ml.exercise.06.01.png", dpi=300)
+
+
+training_data = []
+
+for inx, iny in zip(x_points_1, y_points_1):
+    training_data.append((inx, iny, 0))
+
+for inx, iny in zip(x_points_2, y_points_2):
+    training_data.append((inx, iny, 1))
+
+for inx, iny in zip(x_points_1, y_points_1):
+    training_data.append((inx, iny, 2))
+
+centroids = k_mean(3, training_data)
+
+for centroid in centroids:
+    plt.scatter(centroid[0], centroid[1])
 plt.savefig("ml.exercise.06.01.png", dpi=300)
